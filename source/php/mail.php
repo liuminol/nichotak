c01a74d7f11c32866bdae54130a341ab61ada2bd
liuminol <aplastyr@yandex.ru>
2019-10-14 23:51:57 +0300

    добавлен вывод сообщения при отправке формы

--- source/php/mail.php
+++ source/php/mail.php
@@ -8,4 +8,12 @@
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
     mail($to, $tema, $message, $headers);
+
+    header('Refresh: 5; URL=https://nichotak.000webhostapp.com/');
+    echo ('<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body><h1>Ваше сообщение отправлено!</h1>
+    <p>Вскоре мы с вами свяжемся<br>
+    Через 5 секунд вы вернетесь на главную страницу.</body>');
+    $title = 'Сообщение отправлено';
 ?>
